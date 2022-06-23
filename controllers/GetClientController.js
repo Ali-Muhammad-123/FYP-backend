c9934a40a3c75f6afd18824e238bf01715db173a
nouman41307 <noumansaleem1999@gmail.com>
2022-06-23 12:04:25 +0500

    added

--- controllers/GetClientController.js
+++ controllers/GetClientController.js
@@ -1,56 +1,44 @@
 const Client = require("../models/Client");
 
 class GetClientController {
+  static async Execute(req, res) {
+    const { id } = req.query;
 
-    static async Execute(req, res) {
-
-        const { id } = req.query
-
-        if (id != undefined) {
-
-
-            const client = Client.findById(id, function (err, result) {
-                if (err) {
-                    res.status(400).send(err);
-                } else {
-
-                    if (result) {
-                        res.status(200).send({
-                            message: "Successfull",
-                            _id: result._id,
-                            email: result.email,
-                            firstName: result.firstName,
-                            lastName: result.lastName
-                        });
-                    } else {
-                        res.status(400).send({
-                            message: "No records found!"
-                        });
-                    }
-
-
-                }
-
-            });
-
+    if (id != undefined) {
+      const client = Client.findById(id, function (err, result) {
+        if (err) {
+          res.status(400).send(err);
         } else {
-            const client = await Client.find().select("_id, email, firstName, lastName");
-
-            if (client && client.length > 0) {
-                console.log(client);
-                res.status(200).send({
-                    client: client
-                });
-            } else {
-                res.status(400).send({
-                    message: "No records found!"
-                });
-            }
+          if (result) {
+            res.status(200).send({
+              message: "Successfull",
+              _id: result._id,
+              email: result.email,
+              firstName: result.firstName,
+              lastName: result.lastName,
+            });
+          } else {
+            res.status(400).send({
+              message: "No records found!",
+            });
+          }
         }
+      });
+    } else {
+      const client = await Client.find().select("_id email firstName lastName");
 
-
+      if (client && client.length > 0) {
+        console.log(client);
+        res.status(200).send({
+          client: client,
+        });
+      } else {
+        res.status(400).send({
+          message: "No records found!",
+        });
+      }
     }
+  }
 }
-
 
 module.exports = GetClientController;
