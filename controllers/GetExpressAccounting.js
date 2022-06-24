51a9b5b1cc60c9e13375bf3ffac496abee875acf
Zabloon <zabloona@gmail.com>
2022-06-24 17:15:07 +0500

    added get express accounting request

---
+++ controllers/GetExpressAccounting.js
@@ -0,0 +1,61 @@
+const ExpressAccountingRequest = require("../models/expressAccountingRequest");
+
+class GetExpressAccountingRequestController {
+
+    static async Execute(req, res) {
+
+        const { user } = req.body;
+
+        if (user != undefined) {
+
+
+
+            var expressAccountingRequest = await ExpressAccountingRequest.find({
+
+                user: user
+            }).populate({
+                path: 'user',
+                select:
+                    'firstName lastName',
+            });
+
+            if (expressAccountingRequest && expressAccountingRequest.length > 0) {
+
+                res.status(200).json({
+                    message: "Sucess",
+                    expressAccountingRequest: expressAccountingRequest
+                });
+
+            } else {
+                res.status(404).json({
+                    message: "No Record found",
+                });
+            }
+
+
+        } else {
+
+            var expressAccountingRequest = await ExpressAccountingRequest.find().populate({
+                path: 'user',
+                select:
+                    'firstName lastName',
+            });
+
+            if (expressAccountingRequest && expressAccountingRequest.length > 0) {
+
+                res.status(200).json({
+                    message: "Sucess",
+                    expressAccountingRequest: expressAccountingRequest
+                });
+
+            } else {
+                res.status(404).json({
+                    message: "No Record found",
+                });
+            }
+        }
+
+    }
+}
+
+module.exports = GetExpressAccountingRequestController
--- routes/expressAccountingRequest.js
+++ routes/expressAccountingRequest.js
@@ -1,6 +1,7 @@
 const Router = require("express").Router();
 const auth = require("../middleware/adminAuth");
 const PostExpressAccounting = require("../controllers/PostExpressAccounting");
+const GetExpressAccounting = require("../controllers/GetExpressAccounting");
 
 
 Router.post('/expressaccounting', auth, async (req, res) => {
@@ -8,6 +9,10 @@ Router.post('/expressaccounting', auth, async (req, res) => {
     PostExpressAccounting.Execute(req, res);
 
 
+});
+
+Router.get("/expressaccounting", auth, async (req, res) => {
+    GetExpressAccounting.Execute(req, res);
 });
 
 
