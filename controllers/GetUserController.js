e78f5a3e80900fa7873c261ba438674e7ecd166c
nouman41307 <noumansaleem1999@gmail.com>
2022-06-27 14:51:20 +0500

    added

--- controllers/GetUserController.js
+++ controllers/GetUserController.js
@@ -1,54 +1,45 @@
 const User = require("../models/user");
 
 class GetUserController {
+  static async Execute(req, res) {
+    const { id } = req.query;
 
-    static async Execute(req, res) {
-
-        const { id } = req.query
-
-        if (id != undefined) {
-
-
-            const user = User.findById(id, function (err, result) {
-                if (err) {
-                    res.status(400).send(err);
-                } else {
-
-                    if (result) {
-                        res.status(200).send({
-                            message: "Successfull",
-                            _id: result._id,
-                            email: result.email,
-                            firstName: result.firstName,
-                            lastName: result.lastName
-                        });
-                    } else {
-                        res.status(403).send({
-                            message: "No records found!"
-                        });
-                    }
-
-
-                }
-
-            });
-
+    if (id != undefined) {
+      const user = User.findById(id, function (err, result) {
+        if (err) {
+          res.status(400).send(err);
         } else {
-            const user = await User.find().select("_id email firstName lastName companyName");
-
-            if (user && user.length > 0) {
-                res.status(200).send({
-                    user: user
-                });
-            } else {
-                res.status(403).send({
-                    message: "No records found!"
-                });
-            }
+          if (result) {
+            res.status(200).send({
+              message: "Successfull",
+              _id: result._id,
+              email: result.email,
+              firstName: result.firstName,
+              lastName: result.lastName,
+            });
+          } else {
+            res.status(403).send({
+              message: "No records found!",
+            });
+          }
         }
-    };
+      });
+    } else {
+      const user = await User.find().select(
+        "_id email firstName lastName companyName"
+      );
+
+      if (user && user.length > 0) {
+        res.status(200).send({
+          user: user,
+        });
+      } else {
+        res.status(403).send({
+          message: "No records found!",
+        });
+      }
+    }
+  }
 }
-
-
 
 module.exports = GetUserController;
