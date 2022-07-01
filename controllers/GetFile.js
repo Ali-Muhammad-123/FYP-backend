4bba585b75b8bd42abd9f4e35523ee764c9d1a04
Zabloon <zabloona@gmail.com>
2022-07-01 18:27:55 +0500

    small changes

--- controllers/GetFile.js
+++ controllers/GetFile.js
@@ -9,7 +9,7 @@ class GetFile {
     if (id != undefined) {
       const file = await File.find({ _id: id });
 
-      if (file && file[0].file && file[0].docOF) {
+      if (file[0] && file[0].file && file[0].docOF) {
         var fileObt = fs.readFileSync(
           path.resolve(__dirname, `..${file[0].docOF}/${file[0].file}`)
         );
