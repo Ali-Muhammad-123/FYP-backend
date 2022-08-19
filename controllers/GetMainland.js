d7cc56492d379057daf6da90ce7e9aa8b00e0cef
Zabloon <zabloona@gmail.com>
2022-08-19 22:46:13 +0500

    added

---
+++ controllers/GetMainland.js
@@ -0,0 +1,52 @@
+const Mainland = require("../models/mainland");
+
+class GetMainlandController {
+
+    static async Execute(req, res) {
+
+        const { id } = req.query;
+
+        if (id != undefined && id.match(/^[0-9a-fA-F]{24}$/)) {
+
+
+
+            var mainland = await Mainland.find({
+                emirates_id: id
+            });
+
+            if (mainland && mainland.length > 0) {
+
+                res.status(200).json({
+                    message: "Sucess",
+                    mainland: mainland
+                });
+
+            } else {
+                res.status(403).json({
+                    message: "No Record found",
+                });
+            }
+
+
+        } else {
+
+            var mainland = await Mainland.find();
+
+            if (mainland && mainland.length > 0) {
+
+                res.status(200).json({
+                    message: "Sucess",
+                    mainland: mainland
+                });
+
+            } else {
+                res.status(403).json({
+                    message: "No Record found",
+                });
+            }
+        }
+
+    }
+}
+
+module.exports = GetMainlandController
