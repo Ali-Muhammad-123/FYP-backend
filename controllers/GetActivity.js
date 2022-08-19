c0d4209d5fff211e36738f1c20be5c57e6bf246e
Ali-Muhammad-123 <Alim.9171@gmail.com>
2022-08-20 00:33:40 +0500

    added

--- controllers/GetActivity.js
+++ controllers/GetActivity.js
@@ -1,52 +1,43 @@
 const Activity = require("../models/activity");
 
 class GetActivityController {
+  static async Execute(req, res) {
+    const { id } = req.query;
 
-    static async Execute(req, res) {
+    if (id != undefined && id.match(/^[0-9a-fA-F]{24}$/)) {
+      var activity = await Activity.find({
+        mainland_id: id,
+      }).populate({
+        path: "mainland_id",
+      });
 
-        const { id } = req.query;
+      if (activity && activity.length > 0) {
+        res.status(200).json({
+          message: "Sucess",
+          activity: activity,
+        });
+      } else {
+        res.status(403).json({
+          message: "No Record found",
+        });
+      }
+    } else {
+      var activity = await Activity.find().populate({
+        path: "mainland_id",
+      });
 
-        if (id != undefined && id.match(/^[0-9a-fA-F]{24}$/)) {
-
-
-
-            var activity = await Activity.find({
-                mainland_id: id
-            });
-
-            if (activity && activity.length > 0) {
-
-                res.status(200).json({
-                    message: "Sucess",
-                    activity: activity
-                });
-
-            } else {
-                res.status(403).json({
-                    message: "No Record found",
-                });
-            }
-
-
-        } else {
-
-            var activity = await Activity.find();
-
-            if (activity && activity.length > 0) {
-
-                res.status(200).json({
-                    message: "Sucess",
-                    activity: activity
-                });
-
-            } else {
-                res.status(403).json({
-                    message: "No Record found",
-                });
-            }
-        }
-
+      if (activity && activity.length > 0) {
+        res.status(200).json({
+          message: "Sucess",
+          activity: activity,
+        });
+      } else {
+        res.status(403).json({
+          message: "No Record found",
+        });
+      }
     }
+  }
 }
 
-module.exports = GetActivityController
+module.exports = GetActivityController;
