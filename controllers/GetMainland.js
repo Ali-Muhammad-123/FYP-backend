6ec3d555f6925d4ee1f4aece46ad1c4d91cf4dd5
Ali-Muhammad-123 <Alim.9171@gmail.com>
2022-08-20 17:22:04 +0500

    added

--- controllers/GetMainland.js
+++ controllers/GetMainland.js
@@ -1,52 +1,43 @@
 const Mainland = require("../models/mainland");
 
 class GetMainlandController {
+  static async Execute(req, res) {
+    const { id } = req.query;
 
-    static async Execute(req, res) {
+    if (id != undefined && id.match(/^[0-9a-fA-F]{24}$/)) {
+      var mainland = await Mainland.find({
+        emirates_id: id,
+      }).populate({
+        path: "emirates_id",
+      });
 
-        const { id } = req.query;
+      if (mainland && mainland.length > 0) {
+        res.status(200).json({
+          message: "Sucess",
+          mainland: mainland,
+        });
+      } else {
+        res.status(403).json({
+          message: "No Record found",
+        });
+      }
+    } else {
+      var mainland = await Mainland.find().populate({
+        path: "emirates_id",
+      });
 
-        if (id != undefined && id.match(/^[0-9a-fA-F]{24}$/)) {
-
-
-
-            var mainland = await Mainland.find({
-                emirates_id: id
-            });
-
-            if (mainland && mainland.length > 0) {
-
-                res.status(200).json({
-                    message: "Sucess",
-                    mainland: mainland
-                });
-
-            } else {
-                res.status(403).json({
-                    message: "No Record found",
-                });
-            }
-
-
-        } else {
-
-            var mainland = await Mainland.find();
-
-            if (mainland && mainland.length > 0) {
-
-                res.status(200).json({
-                    message: "Sucess",
-                    mainland: mainland
-                });
-
-            } else {
-                res.status(403).json({
-                    message: "No Record found",
-                });
-            }
-        }
-
+      if (mainland && mainland.length > 0) {
+        res.status(200).json({
+          message: "Sucess",
+          mainland: mainland,
+        });
+      } else {
+        res.status(403).json({
+          message: "No Record found",
+        });
+      }
     }
+  }
 }
 
-module.exports = GetMainlandController
+module.exports = GetMainlandController;
