c3d01c91827e4d345e130be6874553dd90c133f8
Ali <alim.9171@gmail.com>
2022-08-24 19:28:33 +0500

    'added'

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -4,54 +4,54 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 class LoginController {
-  static async Execute(req, res) {
-    console.log(req.body);
-    const { email, password } = req.body;
+	static async Execute(req, res) {
+		console.log(req.body);
+		const { email, password } = req.body;
 
-    if (email != undefined && password != undefined) {
-      const existingUser = await Credential.findOne({
-        email: email,
-      });
-      console.log(existingUser);
-      if (existingUser) {
-        const user = await User.find({ _id: existingUser.user });
+		if (email != undefined && password != undefined) {
+			const existingUser = await Credential.findOne({
+				email: email.toLowerCase(),
+			});
+			console.log(existingUser);
+			if (existingUser) {
+				const user = await User.find({ _id: existingUser.user });
 
-        console.log(user);
-        await bcrypt
-          .compare(password, existingUser.password)
-          .then(function (result) {
-            if (result == true) {
-              const token = jwt.sign(
-                JSON.stringify({
-                  _id: existingUser._id,
-                  role: existingUser.role,
-                }),
-                process.env.ACCESS_TOKEN_JWT
-              );
-              res.setHeader("x-auth-token", token);
-              res.status(200).send({
-                message: "Login Successfull",
-                email: existingUser.email,
-                _id: user[0]._id,
-                token: token,
-              });
-            } else {
-              res.status(400).send({
-                message: "Invaild credentials",
-              });
-            }
-          });
-      } else {
-        res.status(403).send({
-          message: "No user found",
-        });
-      }
-    } else {
-      res.status(400).send({
-        message: "Invalid request",
-      });
-    }
-  }
+				console.log(user);
+				await bcrypt
+					.compare(password, existingUser.password)
+					.then(function (result) {
+						if (result == true) {
+							const token = jwt.sign(
+								JSON.stringify({
+									_id: existingUser._id,
+									role: existingUser.role,
+								}),
+								process.env.ACCESS_TOKEN_JWT
+							);
+							res.setHeader("x-auth-token", token);
+							res.status(200).send({
+								message: "Login Successfull",
+								email: existingUser.email,
+								_id: user[0]._id,
+								token: token,
+							});
+						} else {
+							res.status(400).send({
+								message: "Invaild credentials",
+							});
+						}
+					});
+			} else {
+				res.status(403).send({
+					message: "No user found",
+				});
+			}
+		} else {
+			res.status(400).send({
+				message: "Invalid request",
+			});
+		}
+	}
 }
 
 module.exports = LoginController;
